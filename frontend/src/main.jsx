b7a5de4880ba3f8137fde2bc3b840d1751daa80e
Eep Ridwan Pauji <erpauji.medic@gmail.com>
2026-07-11 22:28:52 +0700

    fix: add buffer polyfill for otplib and update deprecated meta tags

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Buffer } from 'buffer'
+if (typeof window !== 'undefined') {
+  window.Buffer = window.Buffer || Buffer
+}
 import App from './App.jsx'
 import './index.css'
 
